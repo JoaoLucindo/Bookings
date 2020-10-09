f1885f6a5c0a65753e10b93fabc69728524e8125
JoaoLucindo <jflucindo@outlook.com>
2020-10-09 11:40:21 -0300

    Add files via upload

---
+++ MS Booking - Holiday.ps1
@@ -0,0 +1,53 @@
+﻿$HolidayStart = @"
+{
+        "@odata.type":"#microsoft.graph.dateTimeTimeZone",
+        "dateTime":"2020-11-14T00:00:01.0000000+00:00",
+        "timeZone":"UTC"
+    }
+"@
+$HolidayEnd = @"
+{
+        "@odata.type":"#microsoft.graph.dateTimeTimeZone",
+        "dateTime":"2020-11-14T23:59:59.0000000+00:00",
+        "timeZone":"UTC"
+    }
+"@
+
+Connect-MgGraph -Scopes "Bookings.Read.All", "BookingsAppointment.ReadWrite.All", "Bookings.ReadWrite.All", "Bookings.Manage.All"
+
+# Get-command -module *booking* | select CommanType, Name| more
+
+Select-MgProfile "Beta"
+
+#get all available bookings
+$BusinessBookings = (Get-MgBookingBusiness | Select id) 
+
+foreach ($BusinessBookings_ID in $BusinessBookings) {
+
+Write-Host "------------------------------------------" -ForegroundColor Green
+
+Write-Host "Current BusinessBookings:" $BusinessBookings_ID.Id -ForegroundColor Green
+
+
+#get all staff member for the current Business Booking
+
+$Staff = (Get-MgBookingBusinessStaffMember -BookingBusinessId $BusinessBookings_ID.Id | Select id)  | Foreach {"$($_.id)"}
+
+Write-Host "All staff members Id:" $Staff -ForegroundColor Green
+
+#get all services available for the current Business Booking
+$Service = (Get-MgBookingBusinessService -BookingBusinessId $BusinessBookings_ID.Id | Select id) 
+
+Write-Host "First Service ID available" $Service[0].id -ForegroundColor Green
+
+
+#Create a new booking appointment
+
+$NewBooking = New-MgBookingBusinessAppointment -BookingBusinessId $BusinessBookings_ID.Id -StaffMemberIds $Staff -Start $HolidayStart -End $HolidayEnd -ServiceId $Service[0].id;
+
+
+if($NewBooking){
+Write-Host "Business Appointment has been created successfully, Id:" $NewBooking.Id -ForegroundColor Green
+}
+
+}
